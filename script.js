126dafdd0dbcbb90d85c00fbc91b4d45dfdd33b7
itpRakib <rakibulislam.cse.41230301525@gmail.com>
2024-11-30 04:51:30 +0600

    n

--- script.js
+++ script.js
@@ -26,37 +26,37 @@ let note = {
     y: null
 }
 
-document.addEventListener('mousedown',(event)=>{
-    if(event.target.classList.contains('note')){
-        cursor={
-            x:event.clientX,
-            y:event.clientY
-        }  
-        note={
-            dom:event.target,
-            x:event.target.getBoundingClientRect().left,
-            y:event.target.getBoundingClientRect().top
+document.addEventListener('mousedown', (event) => {
+    if (event.target.classList.contains('note')) {
+        cursor = {
+            x: event.clientX,
+            y: event.clientY
+        }
+        note = {
+            dom: event.target,
+            x: event.target.getBoundingClientRect().left,
+            y: event.target.getBoundingClientRect().top
         }
         console.table(cursor)
-      }
+    }
 })
 
-document.addEventListener('mousemove', (event)=>{
-    if(note.dom==null)return;
-    let currentCursor={
-        x:event.clientX,
-        y:event.clientY
+document.addEventListener('mousemove', (event) => {
+    if (note.dom == null) return;
+    let currentCursor = {
+        x: event.clientX,
+        y: event.clientY
     }
-    let distance ={
-        x: currentCursor.x-cursor.x,
-        y: currentCursor.y-cursor.y
+    let distance = {
+        x: currentCursor.x - cursor.x,
+        y: currentCursor.y - cursor.y
     }
-    note.dom.style.left=(note.x+distance.x)+'px';
-    note.dom.style.top=(note.y+distance.y)+'px';
+    note.dom.style.left = (note.x + distance.x) + 'px';
+    note.dom.style.top = (note.y + distance.y) + 'px';
 })
 
-document.addEventListener('mouseup',()=>{
-    if(note.dom == null) return;
-    note.dom.style.cursor='auto';
-    note.dom=null;
+document.addEventListener('mouseup', () => {
+    if (note.dom == null) return;
+    note.dom.style.cursor = 'auto';
+    note.dom = null;
 })
